04ac766cd7f7e6ab1d0f145d8c737e277dab9b77
TanyaSoni2912 <tanu139sona@gmail.com>
2024-11-22 15:09:35 +0530

    Route for auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,12 @@
+/** @format */
+
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
+const { authenticateToken } = require('../middleware/auth.middleware');
 
-router.post('/signin', authController.signIn);
+router.post('/register', authController.signIn);
+router.post('/login', authController.login);
+router.post('/me', authenticateToken, authController.me);
 
 module.exports = router;
